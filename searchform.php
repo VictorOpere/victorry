e38e0b31fc9822c61b3f72ff804343fd19223848
Victor Opere <victoropere@gmail.com>
2020-09-20 14:31:41 +0300

    just finished this theme

---
+++ searchform.php
@@ -0,0 +1,8 @@
+<div class="input-group mb-3">
+<form class="Search" method="get" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+  <input type="text" class="form-control" placeholder="Search Keyword" aria-label="Search" aria-describedby="button-addon2">
+  <div class="input-group-append">
+    <button class="btn btn-outline-danger" type="button" id="button-addon2">Button</button>
+  </div>
+</form>
+</div>
